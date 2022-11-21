40fc3ea404aa109beac461e15d2bd2744f5808b8
JohnnySquirrelCat <118567977+JohnnySquirrelCat@users.noreply.github.com>
2022-11-21 11:12:01 -0500

    finished step 3
    finished step 3
    
    lets see if the code is correct?

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +10,29 @@ namespace GradeBook.GradeBooks
     public RankedGradeBook(string name) : base(name)
     {
       Type = Enums.GradeBookType.Ranked;
+    }
+
+    public override char GetLetterGrade(double averageGrade)
+    {
+      
+      if (Students.Count < 5)
+      {
+        throw new InvalidOperationException(" Ranked-grading requires a minimum of 5 students to work please add more students. ");
+      }
+
+      int threshold = (int)Math.Ceiling(Students.Count * 0.2);
+      List<double> grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+
+      if (averageGrade >= grades[threshold - 1])
+        return 'A';
+      if (averageGrade >= grades[(threshold * 2) - 1])
+        return 'B';
+      if (averageGrade >= grades[(threshold * 3) - 1])
+        return 'C';
+      if (averageGrade >= grades[(threshold * 4) - 1])
+        return 'D';
+
+      return 'F';
     }
 
   }
